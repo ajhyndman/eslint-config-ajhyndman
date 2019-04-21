b0bd08c2cc2e35a80238c1e88ead619e19794c17
Andrew Hyndman <ajhyndman@hotmail.com>
2019-04-21 15:16:23 -0700

    Remove conflicting eslint rule

--- flow.js
+++ flow.js
@@ -1,20 +1,11 @@
 module.exports = {
   "extends": "ajhyndman",
-  "plugins": [
-    "flowtype"
-  ],
+  "plugins": ["flowtype"],
   "rules": {
     // eslint-plugin-flow
-    "flowtype/object-type-delimiter": ["error", "semicolon"],
     "flowtype/require-valid-file-annotation": ["error", "always"],
     "flowtype/semi": "error",
-    "flowtype/space-after-type-colon": [
-      "error",
-      "always"
-    ],
-    "flowtype/space-before-type-colon": [
-      "error",
-      "never"
-    ],
+    "flowtype/space-after-type-colon": ["error", "always"],
+    "flowtype/space-before-type-colon": ["error", "never"],
   }
 };
